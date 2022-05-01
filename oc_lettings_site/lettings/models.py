3b2dd9e7ad8ad022953017d406ca7ea248255ad5
Milan Viricel <viricel.milan@gmail.com>
2022-05-01 11:48:52 +0200

    Restructuration de l'architecture modulaire

--- oc_lettings_site/lettings/models.py
+++ oc_lettings_site/lettings/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinLengthValidator
-from django.contrib.auth.models import User
 
 
 class Address(models.Model):
@@ -11,12 +10,11 @@ class Address(models.Model):
     zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
     country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
 
-    class Meta:
-        verbose_name_plural = 'Addresses'
-
-
     def __str__(self):
         return f'{self.number} {self.street}'
+
+    class Meta:
+        verbose_name_plural = 'Addresses'
 
 
 class Letting(models.Model):
@@ -25,11 +23,3 @@ class Letting(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    favorite_city = models.CharField(max_length=64, blank=True)
-
-    def __str__(self):
-        return self.user.username
